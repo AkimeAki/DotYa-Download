886c247302acaf156c0cace8365043f73131c91f
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-07-07 22:40:44 +0900

    動作テスト

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,26 @@ import { BetaAnalyticsDataClient } from "@google-analytics/data";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response(env.GOOGLE_ANALYTICS_PROPERTY_ID);
+		const analyticsDataClient = new BetaAnalyticsDataClient();
+
+		const [response] = await analyticsDataClient.runReport({
+			property: `properties/${env.GOOGLE_ANALYTICS_PROPERTY_ID}`,
+			dateRanges: [
+				{
+					startDate: "2021-01-01",
+					endDate: "today",
+				},
+			],
+			metrics: [
+				{
+					name: "activeUsers",
+				},
+			],
+		});
+
+		console.log("Report result:");
+		console.log(response);
+
+		return new Response("aaa");
 	},
 } satisfies ExportedHandler<Env>;

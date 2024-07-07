11bdec2d2bb5d7bbc4425166e03b036a42414824
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-07-07 21:57:38 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+		return new Response("Hello Worldtest");
 	},
 } satisfies ExportedHandler<Env>;

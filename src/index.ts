25ccd9370e8090e7ea590869000b7c08688eccb8
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-07-07 22:42:23 +0900

    動作テスト

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,23 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const analyticsDataClient = new BetaAnalyticsDataClient();
 
-		const [response] = await analyticsDataClient.runReport({
-			property: `properties/${env.GOOGLE_ANALYTICS_PROPERTY_ID}`,
-			dateRanges: [
-				{
-					startDate: "2021-01-01",
-					endDate: "today",
-				},
-			],
-			metrics: [
-				{
-					name: "activeUsers",
-				},
-			],
-		});
+		// const [response] = await analyticsDataClient.runReport({
+		// 	property: `properties/${env.GOOGLE_ANALYTICS_PROPERTY_ID}`,
+		// 	dateRanges: [
+		// 		{
+		// 			startDate: "2021-01-01",
+		// 			endDate: "today",
+		// 		},
+		// 	],
+		// 	metrics: [
+		// 		{
+		// 			name: "activeUsers",
+		// 		},
+		// 	],
+		// });
 
-		console.log("Report result:");
-		console.log(response);
+		// console.log("Report result:");
+		// console.log(response);
 
 		return new Response("aaa");
 	},

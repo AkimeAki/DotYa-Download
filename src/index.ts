4fc9b60454d367c8d01dad3c85af9915ceff44ca
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-07-07 22:08:51 +0900

    環境変数テスト

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { BetaAnalyticsDataClient } from "@google-analytics/data";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response(process.env.GOOGLE_ANALYTICS_PROPERTY_ID);
+		return new Response(env.GOOGLE_ANALYTICS_PROPERTY_ID);
 	},
 } satisfies ExportedHandler<Env>;
